261258cc4431642acb6710d648bd519b266d3a5a
Homayoun <homayoun763@gmail.com>
2024-03-30 14:27:08 +0330

    zero client

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span> Interview Questions</span>,
+  logo: <span> Royal Web Doc for developer</span>,
   project: {
     link: 'https://github.com/homayunmmdy',
   },
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: ' Royal Web Doc',
   },
 }
 
